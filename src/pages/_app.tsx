3b47b87f26fd5283799f82fe72df994c759490c9
TrueCare <truecare-geral@outlook.pt>
2025-08-11 16:39:10 +0100

    Development updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import '../styles/styles.css'; // importa o CSS global
-import { appWithTranslation } from "next-i18next"; // importa o wrapper
+import '../styles/styles.css';
+import { appWithTranslation } from "next-i18next";
 import SEOHead from '@/components/SEOHead';
-import '@/styles/globals.css';
+import { Analytics } from "@vercel/analytics/react"; // aqui o import correto
 
 function App({ Component, pageProps }: AppProps) {
-  return (<>
-    <SEOHead />
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <SEOHead />
+      <Component {...pageProps} />
+      <Analytics /> {/* aqui o componente do Vercel Analytics */}
+    </>
   );
 }
 
