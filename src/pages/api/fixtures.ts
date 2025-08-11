bc2880eea0f4af63621f48227d8305f46c338017
TrueCare <truecare-geral@outlook.pt>
2025-08-11 14:00:21 +0100

    Development changes

--- src/pages/api/fixtures.ts
+++ src/pages/api/fixtures.ts
@@ -119,7 +119,7 @@ export default async function handler(
     setToCache(cacheKey, limitedFixtures, 3600);
 
     res.status(200).json(limitedFixtures);
-  } catch (_error) {
+  } catch {
     res.status(500).json({ error: 'Server internal error.' });
   }
 }
